d639a2f9390cfbf9b6dd1aca10861efd3e1556f5
Dmitriy Nikishin <51823084+nik-di@users.noreply.github.com>
2020-09-22 19:39:14 +0300

    1.Adjusted the proxy for the fallback api; 2.Fixed the link in the footer

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,4 +1,4 @@
 export const NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything';
 export const NEWS_EXPLORER_BASE_URL = 'https://api.news-explorer.website';
-export const PRAKTIKUM_PROXY_SERVER = 'https://praktikum.tk/news/v2/everything';
+export const PRAKTIKUM_PROXY_SERVER = 'https://nomoreparties.co/news/v2/everything';
 export const NEWS_API_KEY = 'fd33d47d86e44ccf9ecdf35978f685ad';
